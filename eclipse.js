ebcf54bc5468335baa8eeea16d452fc3c668c6b4
Gilberto Papa <gilberto.papa@digiage.com.br>
2019-05-11 22:49:14 -0300

    first commit

---
+++ eclipse.js
@@ -0,0 +1,69 @@
+Tabela 1. Navegação
+
+Atalho	Descrição
+Ctrl + Shift + R	Dialog de pesquisa para recursos, ex: arquivos de texto, classes java, arquivos html, etc.
+Ctrl + Shift + T	Dialog de pesquisa por tipo
+Ctrl + E	Dialog de pesquisa para selecionar algum arquivo atualmente aberto
+Ctrl + F8	Atalho para trocar de pespectiva
+Tabela 2 Pesquisa no editor
+
+Atalho	Descrição
+F3	Abre o editor para o elemento selecionado (TIPO) ou navega para a declaração da variavel selecionada.
+Ctrl + .	Vai para o próximo problema / error
+Ctrl + ,	Vai para o problem / error anterior
+F4 na variável	Exibe a Hierarquia do tipo
+Ctrl + K	Procura pelo texto selecionado ou se nada estiver selecionado procura pela ultima pesquisa do Find dialog.
+Ctrl + Shift + G	No editor java, pesquisa por referencias no workspace
+Ctrl + Shift + P	Seleciona o { ou } do seu codigo java. O cursor precisa estar entre as "{}" (Chaves).
+Tabela 3. Navegação entre editores
+
+Atalho	Descrição
+Alt + ←	Vai para o editor anterior. O cursor é colocado no local onde esta antes de abrir o novo editor.
+Alt + →	Similar ao Alt + ← mas abre o próximo editor
+Ctrl + Q	Vai para o editor e posição do ultimo editor que foi feito alteções.
+Ctrl + PageUp	Vai para o editor anterior aberto
+Ctrl + PageDown	Vai para o próximo editor aberto
+Tabela 4. Executando programas
+
+Atalho	Descrição
+Ctrl + F11	Executar
+Alt + Shift + X, J	Executa classe selecionada como Aplicaçao Java
+
+A seguinte lista contem atalhos úteis se você esta dentro do seu Java editor.
+
+Tabela 5. Manuseando editor
+
+Atalho	Descrição
+Ctrl + 1	Correção rápida; resultado depende da posição do cursor.
+Ctrl + Space	completa o código que você esta digitando.
+Ctrl + T	Exibe a hierarquia da classe java ou metodo.
+Ctrl + O	Exibe todos os metodos da classe atual, precione Ctrl + O novamente para exibir a hierarquia dos metodos.
+Ctrl + M	Maximiza o editor ativo
+Ctrl + Shift + F	Formata o código fonte.
+Ctrl + I	Corrige a identação, Ex. formata tabs/espaços no código
+Ctrl + F	Abre o dialogo de pesquisa
+Ctrl + Shift + O	Organiza os imports; adiciona as instruções de imports faltando and remove os não usados.
+Tabela 6. Copiar e mover linhas
+
+Atalho	Descrição
+Ctrl + Alt + ↓ ou Ctrl + Alt + ↑	Copia a linha ou seleção em que o cursor está localizado.
+Alt + Up ou Alt + Down	Move a linha ou seleção para cima ou para baixo
+
+Tabela 7. Delete
+
+Atalho	Descrição
+Ctrl + D	Deleta linha ou seleção
+Ctrl + Shift + DEL	Deleta a linha do lado direito do cursor
+Ctrl + DEL	Deleta o próximo elemento
+Ctrl + BACKSPACE	Deleta o elemento anterior
+Tabela 8. Criar novas linhas
+
+Atalho	Descrição
+Shift + Enter	Adiciona uma linha em branco abaixo da linha atual e move o cursor para a nova linha. A diferença entre o ENTER e esse é que a linha atual não é alterada, independentimente da posição do cursor.
+Ctrl+Shift+Enter	Mesmo que o Shift + Enter mas para cima.
+Tabela 9. Codificação
+
+Atalho	Descrição
+Shift + F2	Exibe o Javadoc para o tipo / class / metodo selecionado
+Alt+Shift + N	Atalho para o menu para criar novos objetos
+Alt + Shift + Z	Criar um block com try e catch no codigo selecionado
---
+++ vscode.js
@@ -0,0 +1,31 @@
+Abrir uma classe / Arquivo: Ctrl + , (vírgula)
+Basta digitar o nome da classe e dar enter, ou selecionar uma das opções apresentadas.
+
+Build do Projeto: Ctrl + Shift + b
+
+Importar classe / pacote: Ctrl + . (ponto)
+Basta digitar o nome da classe e dar Ctrl + . que o using será adicionado automaticamente
+
+Salvar todos os documentos abertos: Ctrl + Shift + s
+
+Alterar para tela cheia: Alt + Shift + Enter
+
+Comentar código selecionado: Ctrl + k + c
+
+‘Descomentar’ código selecionado: Ctrl + k + u
+
+Navegar entre as abas: Ctrl + Tab
+
+Ir para codebehind/View: F7
+
+Abrir o File Dialog: Ctrl + n
+
+Abrir o Project Dialog: Ctrl + Shift + n
+
+Abrir arquivo: Ctrl + o
+
+Selecionar a palavra onde o cursor está apontado: Ctrl + w
+
+Abrir o intellisense: Ctrl + J
+
+Identar código: Ctrl + K + D
